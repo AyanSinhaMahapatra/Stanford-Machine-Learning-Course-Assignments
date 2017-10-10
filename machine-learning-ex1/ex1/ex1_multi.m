4a320a8365cd1b46d9a342900141cb0d9b563fcc
Ayan Sinha Mahapatra <ayansmahapatra@gamil.com>
2017-10-11 04:24:15 +0530

    EX1 Complete Full

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+qwerty = [1 1650 3]
+price = qwerty * theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+qwerty = [1 1650 3]
+price = qwerty * theta; % You should change this
 
 
 % ============================================================
